304b449cfac870471dc46b3af1de36349a86ab71
gunsal15 <gunsal15@ku.edu.tr>
2020-01-02 19:13:52 +0300

    implemented stage 2

---
+++ generator.py
@@ -0,0 +1,142 @@
+from .layers import *
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        ch = 48
+        input_channels = 5
+
+        # stage_1
+        self.conv1 = GatedConv2D(input_channels, ch, kernel_size=5)
+        self.conv2_downsample = GatedConv2D(ch, 2 * ch, stride=2)
+        self.conv3 = GatedConv2D(2 * ch, 2 * ch)
+        self.conv4_downsample = GatedConv2D(2 * ch, 4 * ch, stride=2)
+        self.conv5 = GatedConv2D(4 * ch, 4 * ch)
+        self.conv6 = GatedConv2D(4 * ch, 4 * ch)
+        # mask s but its resized in ours
+        self.conv7_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=2)
+        self.conv8_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=4)
+        self.conv9_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=8)
+        self.conv10_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=16)
+        self.conv11 = GatedConv2D(4 * ch, 4 * ch)
+        self.conv12 = GatedConv2D(4 * ch, 4 * ch)
+        self.conv13_upsample = GatedDeconv2D(4 * ch, 2 * ch)
+        self.conv14 = GatedConv2D(2 * ch, 2 * ch)
+        self.conv15_upsample = GatedDeconv2D(2 * ch, ch)
+        self.conv16 = GatedConv2D(ch, ch // 2)
+        self.conv17 = GatedConv2D(ch // 2, 3)
+
+        #stage 2
+        #TODO: reverse mask in here or
+        self.xconv1 = GatedConv2D(ch, ch, 5,1)
+        self.xconv2_downsample = GatedConv2D(ch, ch, 3, 2)
+        self.xconv3 = GatedConv2D(ch, 2 * ch)
+        self.xconv4_downsample = GatedConv2D(2 * ch, 2 * ch, 3, 2)
+        self.xconv5 = GatedConv2D(2 * ch,4 * ch)
+        self.xconv6 = GatedConv2D(4 * ch, 4 * ch)
+        self.xconv7_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=2)
+        self.xconv8_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=4)
+        self.xconv9_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=8)
+        self.xconv10_atrous = GatedConv2D(4 * ch, 4 * ch, dilation=16)
+        #x halu = x in forward
+
+        #attention branch
+        self.pmconv1 = GatedConv2D(ch, ch, 5, 1)
+        self.pmconv2_downsample = GatedConv2D(ch, ch, 5, 1)
+        self.pmconv3 = GatedConv2D(ch, ch, 5, 1)
+        self.pmconv4_downsample = GatedConv2D(ch, ch, 5, 1)
+        self.pmconv5 = GatedConv2D(ch, ch, 5, 1)
+        self.pmconv6 = GatedConv2D(ch, ch, 5, 1)
+        #TODO: contectual attention
+        self.pmconv9 = GatedConv2D(ch, ch, 5, 1)
+        self.pmconv10 = GatedConv2D(ch, ch, 5, 1)
+        #pm = x
+
+        #concat xhalu and pm
+
+        self.allconv11 = GatedConv2D(4 * ch, 4 * ch, 3, 1)
+        self.allconv12 = GatedConv2D(4 * ch, 4 * ch, 3, 1)
+        self.allconv13_upsample = GatedDeconv2D(4 * ch, 2 * ch)
+        self.allconv14 = GatedConv2D(2 * ch, 2 * ch, 3, 1)
+        self.allconv15_upsample = GatedDeconv2D(2 * ch, ch)
+        self.allconv16 = GatedConv2D(ch, ch // 2, 3, 1)
+        self.allconv17 = GatedConv2D(ch // 2, 3, 3, 1)
+
+    def forward(self, x, mask):
+        xin = x
+        ones_x = torch.ones_like(x)[:, 0:1, :, :]
+        x = torch.cat([x, ones_x, ones_x * mask], dim=1)
+        x = self.conv1(x)
+        x = self.conv2_downsample(x)
+        x = self.conv3(x)
+        x = self.conv4_downsample(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7_atrous(x)
+        x = self.conv8_atrous(x)
+        x = self.conv9_atrous(x)
+        x = self.conv10_atrous(x)
+        x = self.conv11(x)
+        x = self.conv12(x)
+        x = self.conv13_upsample(x)
+        x = self.conv14(x)
+        x = self.conv15_upsample(x)
+        x = self.conv16(x)
+        x = self.conv17(x)
+        x = torch.tanh(x)
+        x_stage_1 = x
+
+        x = x*mask + xin[:, 0:3, :, :]*(1-mask)
+        x.reshape(xin[:, 0:3, :, :].shape)
+
+        xnow = x
+        x = self.xconv1(xnow)
+        x = self.xconv2_downsample(x)
+        x = self.xconv3(x)
+        x = self.xconv4_downsample(x)
+        x = self.xconv5(x)
+        x = self.xconv6(x)
+        x = self.xconv7_atrous(x)
+        x = self.xconv8_atrous(x)
+        x = self.xconv9_atrous(x)
+        x = self.xconv10_atrous(x)
+        x_halu = x
+
+        x = self.pmconv1(xnow)
+        x = self.pmconv2_downsample(x)
+        x = self.pmconv3(x)
+        x = self.pmconv4_downsample(x)
+        x = self.pmconv5(x)
+        x = self.pmconv6(x)
+        # TODO: contectual attention
+        x = self.pmconv9(x)
+        x = self.pmconv10(x)
+        pm = x
+
+        x = torch.cat([x_halu,pm],dim=1)
+        x = self.allconv11(x)
+        x = self.allconv12(x)
+        x = self.allconv13_upsample(x)
+        x = self.allconv14(x)
+        x = self.allconv15_upsample(x)
+        x = self.allconv16(x)
+        x = self.allconv17(x)
+        x = torch.tanh(x)
+        x_stage_2 = x
+        #return x_stage_1, x_stage_2, offset_flow
+        return x
+
+
+def main():
+    dtype = torch.float32
+    # number * depth * width * height
+    x = torch.rand((64, 3, 512, 512), dtype=dtype)
+    # mask is  (number of input) * binary matrix
+    mask = torch.zeros((64, 1, 512, 512), dtype=dtype)
+    model = Generator()
+
+
+if __name__ == '__main__':
+    main()
