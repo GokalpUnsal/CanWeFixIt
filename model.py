60532e870aea16ca52faa859e01b0838c0105209
gunsal15 <gunsal15@ku.edu.tr>
2020-01-02 16:29:58 +0300

    gated convs

--- model.py
+++ model.py
@@ -15,10 +15,10 @@ class Model(nn.Module):
         self.conv5 = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1)
         self.conv6 = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1)
         #mask s but its rezied in ours
-        self.conv7_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1,dilation=2)
-        self.conv8_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1,dilation=4)
-        self.conv9_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1,dilation=8)
-        self.conv10_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1,dilation=16)
+        self.conv7_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1, dilation=2)
+        self.conv8_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1, dilation=4)
+        self.conv9_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1, dilation=8)
+        self.conv10_atrous = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1, dilation=16)
         self.conv11 = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1)
         self.conv12 = nn.Conv2d(4*in_channel, 4*in_channel, kernel_size=3, stride=1)
         self.conv13_upsample = nn.ConvTranspose2d(4*in_channel, 2*in_channel, kernel_size=5, stride=2)
@@ -54,11 +54,21 @@ class Model(nn.Module):
         return x
 
     def gate(self,x_in):
-        x,gate = torch.split(x_in,2,3)
+        x,gate = torch.split(x_in,2,1)
         x = F.elu(x)
         gate = torch.sigmoid(gate)
         x = x * gate
         return x
+
+    def gated_conv(self, in_c, out_c, ksize, stride,dil):
+        p = int(dil * (ksize - 1) / 2)
+        x = nn.Conv2d(in_c, out_c, ksize, stride)
+        x = self.gate(x)
+
+    def gated_t_conv(self, in_c, out_c, ksize, stride,dil):
+        p = int(dil * (ksize - 1) / 2)
+        x = nn.Conv2d(in_c, out_c, ksize, stride)
+        x = self.gate(x)
 
 
 def main():
