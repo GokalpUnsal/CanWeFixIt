e0ecb433a6f1dff39edc3cba35fb0453979a930d
BATUHAN YUCER <byucer15@ku.edu.tr>
2020-01-03 04:09:21 +0300

    continued gan training

--- gan.py
+++ gan.py
@@ -21,6 +21,8 @@ class GAN:
         # Beta1 hyperparam for Adam optimizers
         self.beta1 = 0.5
 
+        self.batch_size = 1
+
         # Create batch of latent vectors that we will use to visualize
         #  the progression of the generator
         # fixed_noise = torch.randn(64, nz, 1, 1, device=device)
@@ -30,22 +32,15 @@ class GAN:
 
         # Establish convention for real and fake labels during training
         self.real_label = 1
-        self.fake_label = 0
+        self.fake_label = -1
 
         # Setup Adam optimizers for both G and D
         self.optimizerD = optim.Adam(self.dis.parameters(), lr=self.lr, betas=(self.beta1, 0.999))
         self.optimizerG = optim.Adam(self.gen.parameters(), lr=self.lr, betas=(self.beta1, 0.999))
 
-    # def weights_init(self, m):
-    #     classname = m.__class__.__name__
-    #     if classname.find('Conv') != -1:
-    #         nn.init.normal_(m.conv.weight.data, 0.0, 0.02)
-    #     elif classname.find('BatchNorm') != -1:
-    #         nn.init.normal_(m.conv.weight.data, 1.0, 0.02)
-    #         nn.init.constant_(m.bias.data, 0)
-
-
-    def train(self, dataloader):
+    def train(self, dataset):
+        # Create the dataloader
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
         img_list = []
         G_losses = []
         D_losses = []
@@ -57,13 +52,46 @@ class GAN:
         # img = (img /2) + 0.5
         # plt.imshow(img)
         # plt.show()
-        x = torch.randn((8, 3, 256, 256), dtype=self.dtype, device=self.device)
-        mask = torch.zeros((8, 1, 256, 256), dtype=self.dtype, device=self.device)
+        # x = torch.randn((8, 3, 256, 256), dtype=self.dtype, device=self.device)
+        # mask = torch.zeros((8, 1, 256, 256), dtype=self.dtype, device=self.device)
+
         for epoch in range(self.num_epochs):
-            for i, data in enumerate(dataloader, 0):
+            for i, batch_data in enumerate(dataloader, 0):
+
+                # Prepare batch
+                batch_pos = batch_data
+                # TODO: Random Mask Generation
+                irregular_mask = None
+                mask = irregular_mask
+                batch_incomplete = batch_pos * (1. - mask)
+                xin = batch_incomplete
+
+                # Forward pass for generator
+                self.gen.zero_grad()
+                x1, x2, offset_flow = self.gen(xin, mask)
+                batch_predicted = x2
+                losses = {}
+                # Apply mask and complete image
+                batch_complete = batch_predicted * mask + batch_incomplete * (1. - mask)
+                # TODO: local patches
+                # losses['ae_loss'] = tf.reduce_mean(tf.abs(batch_pos - x1))
+                # losses['ae_loss'] += tf.reduce_mean(tf.abs(batch_pos - x2))
+
+                batch_pos_neg = torch.cat([batch_pos, batch_complete], dim=0)
+                # TODO: torch tile
+                # batch_pos_neg = torch.cat([batch_pos_neg, torch.tile(mask, [self.batch_size * 2, 1, 1, 1])], dim=3)
+                # TODO: hinge loss
+
+                # Forward pass for discriminator
                 self.dis.zero_grad()
-                # print(i)
-                # print(data)
+                pos_neg = self.dis(batch_pos_neg)
+                pos, neg = torch.split(pos_neg, pos_neg.shape[0] // 2)
+                # TODO: error sum
+                # errD_real =
+                # errD_real.backward()
+                # D_x = output.mean().item()
+
+                self.gen.zero_grad()
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -26,21 +26,18 @@ def main():
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
 
-    # Create the dataloader
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-    real_batch = next(iter(dataloader))
-    plt.figure(figsize=(8, 8))
-    plt.axis("off")
-    plt.title("Training Images")
-    a = np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1, 2, 0))
-    plt.imshow(a
-        )
-    plt.show()
+    # real_batch = next(iter(dataloader))
+    # plt.figure(figsize=(8, 8))
+    # plt.axis("off")
+    # plt.title("Training Images")
+    # a = np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1, 2, 0))
+    # plt.imshow(a
+    #     )
+    # plt.show()
 
     # Create GAN
     network = GAN(device)
-    network.train(dataloader)
+    network.train(dataset)
 
 
 if __name__ == '__main__':
