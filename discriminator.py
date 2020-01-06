d3fef1c6c478eac6e59705f18a3dc9dd26bc5577
BATUHAN YUCER <byucer15@ku.edu.tr>
2020-01-07 00:30:30 +0300

    parameter fix

--- discriminator.py
+++ discriminator.py
@@ -5,13 +5,12 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         cnum = 64
-        inp = 256
-        self.conv1 = SpectralConv2D(inp, 3, cnum)
-        self.conv2 = SpectralConv2D(inp, cnum, 2 * cnum)
-        self.conv3 = SpectralConv2D(inp, 2 * cnum, 4 * cnum)
-        self.conv4 = SpectralConv2D(inp, 4 * cnum, 4 * cnum)
-        self.conv5 = SpectralConv2D(inp, 4 * cnum, 4 * cnum)
-        self.conv6 = SpectralConv2D(inp, 4 * cnum, 4 * cnum)
+        self.conv1 = SpectralConv2D(3, cnum)
+        self.conv2 = SpectralConv2D(cnum, 2 * cnum)
+        self.conv3 = SpectralConv2D(2 * cnum, 4 * cnum)
+        self.conv4 = SpectralConv2D(4 * cnum, 4 * cnum)
+        self.conv5 = SpectralConv2D(4 * cnum, 4 * cnum)
+        self.conv6 = SpectralConv2D(4 * cnum, 4 * cnum)
 
     def forward(self, x):
         x = self.conv1(x)
