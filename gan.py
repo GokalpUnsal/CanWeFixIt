e59b654c9fe17142946b5866c2a2ba40697d489a
gunsal15 <gunsal15@ku.edu.tr>
2020-01-07 14:05:12 +0300

    fixed permute in gan

--- gan.py
+++ gan.py
@@ -45,9 +45,9 @@ class GAN:
                 # Prepare batch
                 batch_real = batch_data[0].to(self.device)
                 bbox = random_bbox()
-                regular_mask = bbox2mask(bbox).permute(0, 3, 1, 2)
-                irregular_mask = brush_stroke_mask().permute(0, 3, 1, 2)
-                mask = random.choice([regular_mask, irregular_mask])
+                regular_mask = bbox2mask(bbox)
+                irregular_mask = brush_stroke_mask()
+                mask = random.choice([regular_mask, irregular_mask]).to(self.device)
                 batch_incomplete = batch_real * (torch.tensor(1.) - mask)
                 xin = batch_incomplete.to(self.device)
                 losses = {}
